ad5d652d5880f6d9075343ecfd5b5410706045ca
Adan <adam.kasinski9@gmail.com>
2023-01-30 00:41:39 +0100

    revised altiplano pricing method

--- Options/altiplano_option.jl
+++ Options/altiplano_option.jl
@@ -1,24 +1,41 @@
 using LinearAlgebra, Distributions
 
-function altiplano_option_monte_carlo(C::Float64,mu::Float64, A::Matrix{Float64}, companies::Int64, T::Int64, barrier::Float64, periods::Array{Int64}, S₀::Array{Float64}, K::Float64, 
-    num_of_sim::Int64,r::Float64)
-    
-    rtrn::Array{Float64,1} = zeros(Float32,num_of_sim)
-    for iteration in num_of_sim
-        
-        d = Normal()
-        Z::Matrix{Float64} = rand(d,(companies,T)) 
-        cholesky_matrix::Matrix{Float64} = cholesky(A).L
-        returns::Matrix{Float64} = Z'cholesky_matrix .+mu
-        
-        if any(x->x < barrier, returns./S₀)
-            rtrn[iteration] = max(sum(A[:,periods]./S₀)*ℯ^(-r*T)-K,0)
-        else
-            rtrn[iteration] = C*ℯ^(-r*T)
-        end
 
+function simulate_basket_normal(T, basket_volume, S₀, mu, sigma, correlation_matrix) # TODO: add types
+    # 1) sample vector T x basket volume
+    # 2) calculate Cholesky Matrix
+    # 3) calculate dot product of Cholesky matrix and random sample
+    # 4) generate assets with GBM with correlated sample, mu, sigma of every asset price
+    d = Normal()
+    Z = rand(d,(basket_volume,T))
+    cholesky_matrix = cholesky(correlation_matrix).L
+    delta = Z'cholesky_matrix
+    assets = zeros(basket_volume,T)
+    for asset in 1:basket_volume
+        assets[asset,:] = [S₀[asset] * exp((mu[asset] - 0.5 * sigma[asset]^2) * k + sigma[asset] * sum(delta[1:k-1,asset])) for k in 1:T] # if dt != 1 the formula will be changed
+    end
+    return assets
+end
+
+function price_altiplano_option(T, treshold, r, K, C,periods, basket_volume, S₀, mu, sigma, correlation_matrix)
+    # check if any price fall below a threshold
+    # if True:
+    #   return C
+    # else:
+    #   return Asian style payment
+    
+    basket_prices = simulate_basket_normal(T, basket_volume, S₀, mu, sigma, correlation_matrix)
+    if any(x->x < treshold, basket_prices./S₀)
+        return max(sum(basket_prices[:,periods]./S₀)*ℯ^(-r*T)-K,0)
+    else
+        return C*ℯ^(-r*T)
     end
 
+end
+
+function altiplano_option_monte_carlo(num_of_sim, α, T, treshold, r, K, C,periods, basket_volume, S₀, mu, sigma, correlation_matrix)
+    
+    rtrn::Array{Float64,1} = [price_altiplano_option(T, treshold, r, K, C,periods, basket_volume, S₀, mu, sigma, correlation_matrix) for iteration in 1:num_of_sim]
     θ::Float64 = mean(rtrn)
     s::Float64 = std(rtrn)
     confidence::Float64 = quantile(Normal(), 1-α/2)
@@ -29,5 +46,5 @@ end
  
 cov_matrix = [1.0 0.3 0.4; 0.3 1.0 0.1; 0.4 0.1 1.0] 
 
-println(altiplano_option_monte_carlo(100.0,3.0,cov_matrix,3,10,0.80,[4,6],[1.0,1.0,1.0],30.0,10,0.02)) 
+println(altiplano_option_monte_carlo(1000,0.05,10,1,.02,10,10,[1,3,7],3,[10,10,10],[0.03,0.03,0.03],[0.02,0.02,0.02],cov_matrix)) 
     
